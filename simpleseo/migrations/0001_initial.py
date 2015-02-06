eed5acc7451cc02ff1316e6eb68720ad67d0188e
lampslave <lampslave@gmail.com>
2015-02-06 10:45:38 +0300

    Django 1.7

--- simpleseo/migrations/0001_initial.py
+++ simpleseo/migrations/0001_initial.py
@@ -1,55 +1,37 @@
 # -*- coding: utf-8 -*-
-from south.utils import datetime_utils as datetime
-from south.db import db
-from south.v2 import SchemaMigration
-from django.db import models
+from __future__ import unicode_literals
+
+from django.db import models, migrations
 
 
-class Migration(SchemaMigration):
+class Migration(migrations.Migration):
 
-    def forwards(self, orm):
-        # Adding model 'SeoMetadata'
-        db.create_table('seo_metadata', (
-            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('content_type', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['contenttypes.ContentType'], null=True, blank=True)),
-            ('object_id', self.gf('django.db.models.fields.PositiveIntegerField')(null=True, blank=True)),
-            ('path', self.gf('django.db.models.fields.CharField')(max_length=200, db_index=True)),
-            ('lang_code', self.gf('django.db.models.fields.CharField')(default='ru', max_length=2)),
-            ('title', self.gf('django.db.models.fields.CharField')(max_length=68, blank=True)),
-            ('description', self.gf('django.db.models.fields.CharField')(max_length=155, blank=True)),
-        ))
-        db.send_create_signal(u'simpleseo', ['SeoMetadata'])
+    dependencies = [
+        ('contenttypes', '0001_initial'),
+    ]
 
-        # Adding unique constraint on 'SeoMetadata', fields ['path', 'lang_code']
-        db.create_unique('seo_metadata', ['path', 'lang_code'])
-
-
-    def backwards(self, orm):
-        # Removing unique constraint on 'SeoMetadata', fields ['path', 'lang_code']
-        db.delete_unique('seo_metadata', ['path', 'lang_code'])
-
-        # Deleting model 'SeoMetadata'
-        db.delete_table('seo_metadata')
-
-
-    models = {
-        u'contenttypes.contenttype': {
-            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
-            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
-        },
-        u'simpleseo.seometadata': {
-            'Meta': {'ordering': "('path', 'lang_code')", 'unique_together': "(('path', 'lang_code'),)", 'object_name': 'SeoMetadata', 'db_table': "'seo_metadata'"},
-            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['contenttypes.ContentType']", 'null': 'True', 'blank': 'True'}),
-            'description': ('django.db.models.fields.CharField', [], {'max_length': '155', 'blank': 'True'}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'lang_code': ('django.db.models.fields.CharField', [], {'default': "'ru'", 'max_length': '2'}),
-            'object_id': ('django.db.models.fields.PositiveIntegerField', [], {'null': 'True', 'blank': 'True'}),
-            'path': ('django.db.models.fields.CharField', [], {'max_length': '200', 'db_index': 'True'}),
-            'title': ('django.db.models.fields.CharField', [], {'max_length': '68', 'blank': 'True'})
-        }
-    }
-
-    complete_apps = ['simpleseo']
+    operations = [
+        migrations.CreateModel(
+            name='SeoMetadata',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('object_id', models.PositiveIntegerField(null=True, blank=True)),
+                ('path', models.CharField(help_text="This should be an absolute path, excluding the domain name. Example: '/foo/bar/'.", max_length=255, verbose_name='Path', db_index=True)),
+                ('lang_code', models.CharField(default=b'ru', max_length=2, verbose_name='Language', choices=[(b'af', b'Afrikaans'), (b'ar', b'Arabic'), (b'ast', b'Asturian'), (b'az', b'Azerbaijani'), (b'bg', b'Bulgarian'), (b'be', b'Belarusian'), (b'bn', b'Bengali'), (b'br', b'Breton'), (b'bs', b'Bosnian'), (b'ca', b'Catalan'), (b'cs', b'Czech'), (b'cy', b'Welsh'), (b'da', b'Danish'), (b'de', b'German'), (b'el', b'Greek'), (b'en', b'English'), (b'en-au', b'Australian English'), (b'en-gb', b'British English'), (b'eo', b'Esperanto'), (b'es', b'Spanish'), (b'es-ar', b'Argentinian Spanish'), (b'es-mx', b'Mexican Spanish'), (b'es-ni', b'Nicaraguan Spanish'), (b'es-ve', b'Venezuelan Spanish'), (b'et', b'Estonian'), (b'eu', b'Basque'), (b'fa', b'Persian'), (b'fi', b'Finnish'), (b'fr', b'French'), (b'fy', b'Frisian'), (b'ga', b'Irish'), (b'gl', b'Galician'), (b'he', b'Hebrew'), (b'hi', b'Hindi'), (b'hr', b'Croatian'), (b'hu', b'Hungarian'), (b'ia', b'Interlingua'), (b'id', b'Indonesian'), (b'io', b'Ido'), (b'is', b'Icelandic'), (b'it', b'Italian'), (b'ja', b'Japanese'), (b'ka', b'Georgian'), (b'kk', b'Kazakh'), (b'km', b'Khmer'), (b'kn', b'Kannada'), (b'ko', b'Korean'), (b'lb', b'Luxembourgish'), (b'lt', b'Lithuanian'), (b'lv', b'Latvian'), (b'mk', b'Macedonian'), (b'ml', b'Malayalam'), (b'mn', b'Mongolian'), (b'mr', b'Marathi'), (b'my', b'Burmese'), (b'nb', b'Norwegian Bokmal'), (b'ne', b'Nepali'), (b'nl', b'Dutch'), (b'nn', b'Norwegian Nynorsk'), (b'os', b'Ossetic'), (b'pa', b'Punjabi'), (b'pl', b'Polish'), (b'pt', b'Portuguese'), (b'pt-br', b'Brazilian Portuguese'), (b'ro', b'Romanian'), (b'ru', b'Russian'), (b'sk', b'Slovak'), (b'sl', b'Slovenian'), (b'sq', b'Albanian'), (b'sr', b'Serbian'), (b'sr-latn', b'Serbian Latin'), (b'sv', b'Swedish'), (b'sw', b'Swahili'), (b'ta', b'Tamil'), (b'te', b'Telugu'), (b'th', b'Thai'), (b'tr', b'Turkish'), (b'tt', b'Tatar'), (b'udm', b'Udmurt'), (b'uk', b'Ukrainian'), (b'ur', b'Urdu'), (b'vi', b'Vietnamese'), (b'zh-cn', b'Simplified Chinese'), (b'zh-hans', b'Simplified Chinese'), (b'zh-hant', b'Traditional Chinese'), (b'zh-tw', b'Traditional Chinese')])),
+                ('title', models.CharField(help_text='Recommended length: up to 70 symbols', max_length=255, verbose_name='Title', blank=True)),
+                ('description', models.CharField(help_text='Recommended length: up to 160 symbols.', max_length=255, verbose_name='Description', blank=True)),
+                ('content_type', models.ForeignKey(blank=True, to='contenttypes.ContentType', null=True)),
+            ],
+            options={
+                'ordering': ('path', 'lang_code'),
+                'db_table': 'seo_metadata',
+                'verbose_name': 'SEO metadata',
+                'verbose_name_plural': 'SEO metadata',
+            },
+            bases=(models.Model,),
+        ),
+        migrations.AlterUniqueTogether(
+            name='seometadata',
+            unique_together=set([('path', 'lang_code')]),
+        ),
+    ]
